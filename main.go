2495e92a8f6e5d181dd3f392a6db006d09fdee82
Austin Clements <aclements@csail.mit.edu>
2022-11-29 10:05:23 -0500

    Use higher DPI for PNG plots

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
-	"time"
+	"os"
 
 	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+	"gonum.org/v1/plot/vg/vgimg"
 )
 
 // Notes about SketchUp STL exports:
@@ -31,19 +33,23 @@ func main() {
 	const lat = 42.4195011
 	const lon = -71.2064993
 	const elev = 200
-	var testPos = [3]float64{0, 0, 8 * 12}
-	var cameraOffset = [3]float64{40 * 12, -30 * 12, 10 * 12}
+	var testPos = [3]float64{0, 0, (5 + 5) * 12}
 
 	m := NewShadeModel(lat, lon, elev)
 	m.AddBuildings("house.stl")
 
-	m.Render(testPos, cameraOffset, time.Date(2022, 6, 1, 12, 0, 0, 0, time.Local), "render.png")
+	//var cameraOffset = [3]float64{40 * 12, -30 * 12, 10 * 12}
+	//m.Render(testPos, cameraOffset, time.Date(2022, 6, 1, 12, 0, 0, 0, time.Local), "render.png")
 
 	m.AddFoliage("house-trees.stl")
 
 	plt := m.IntensityOverYear(2022, testPos).HeatMap()
-	err := plt.Save(20*vg.Centimeter, 15*vg.Centimeter, "sun.png")
+	c := vgimg.PngCanvas{Canvas: vgimg.NewWith(vgimg.UseWH(20*vg.Centimeter, 15*vg.Centimeter), vgimg.UseDPI(150))}
+	plt.Draw(draw.New(c))
+	f, err := os.Create("sun.png")
 	if err != nil {
 		log.Panic(err)
 	}
+	defer f.Close()
+	c.WriteTo(f)
 }
